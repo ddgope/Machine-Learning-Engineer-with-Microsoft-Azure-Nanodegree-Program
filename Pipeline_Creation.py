ed8b3f625d07760050c1c3d52f2cd2425ef88df2
Durgadas Gope <44217637+ddgope@users.noreply.github.com>
2021-01-30 21:16:45 -0500

    Pipeline Creation

---
+++ Pipeline_Creation.py
@@ -0,0 +1,30 @@
+# Data Preparation
+ws = Workspace.from_config() 
+blob_store = Datastore(ws, "workspaceblobstore")
+compute_target = ws.compute_targets["STANDARD_NC6"]
+experiment = Experiment(ws, 'MyExperiment') 
+
+input_data = Dataset.File.from_files(
+    DataPath(datastore, '20newsgroups/20news.pkl'))
+
+# Training Configuration
+output_data = PipelineData("output_data", datastore=blob_store)
+
+input_named = input_data.as_named_input('input')
+
+steps = [ PythonScriptStep(
+    script_name="train.py",
+    arguments=["--input", input_named.as_download(),
+        "--output", output_data],
+    inputs=[input_data],
+    outputs=[output_data],
+    compute_target=compute_target,
+    source_directory="myfolder"
+) ]
+
+pipeline = Pipeline(workspace=ws, steps=steps)
+
+
+# Training Configuration
+pipeline_run = experiment.submit(pipeline)
+pipeline_run.wait_for_completion()
